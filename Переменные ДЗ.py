d2fa6023b002962beea4bab5dca6801a752496ac
Z125851 <Z125851@outlook.com>
2024-12-10 12:31:16 +0300

    Add files via upload

---
+++ Переменные ДЗ.py
@@ -0,0 +1,7 @@
+Number_of_homework=12
+Number_of_hours=1.5
+Name_of_course='Python'
+Time_of_task=(Number_of_hours)/(Number_of_homework)
+print(Time_of_task)
+print (Name_of_course, Number_of_homework, Number_of_hours, Time_of_task)
+
